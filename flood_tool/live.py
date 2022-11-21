3866952904e6ddec9c406f0a9f001413cb2cb826
Marijan Beg <m.beg@imperial.ac.uk>
2022-11-21 09:55:09 +0000

    Add ads-deluge/flood_tool/live.py

---
+++ flood_tool/live.py
@@ -0,0 +1,40 @@
+"""Interactions with rainfall and river data."""
+
+import numpy as np
+import pandas as pd
+
+__all__ = ["get_station_data_from_csv"]
+
+
+def get_station_data_from_csv(filename, station_reference):
+    """Return readings for a specified recording station from .csv file.
+
+    Parameters
+    ----------
+
+    filename: str
+        filename to read
+    station_reference
+        station_reference to return.
+
+    >>> data = get_station_data_from_csv('resources/wet_day.csv')
+    """
+    frame = pd.read_csv(filename)
+    frame = frame.loc[frame.stationReference == station_reference]
+
+    return pd.to_numeric(frame.value.values)
+
+def get_live_station_data(filename, station_reference):
+    """Return readings for a specified recording station from live API.
+
+    Parameters
+    ----------
+
+    filename: str
+        filename to read
+    station_reference
+        station_reference to return.
+
+    >>> data = get_live_station_data('0184TH')
+    """
+    return NotImplemented
